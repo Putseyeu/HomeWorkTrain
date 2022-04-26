590f125bae53ee7814d88924bbdb194208c0a678
Putseyeu <putseyeu.a.d@gmail.com>
2022-04-26 17:35:09 +0200

    step2

--- Program.cs
+++ Program.cs
@@ -47,115 +47,137 @@ namespace HomeWorkTrain
         public void ShowInfoTrain()
         {
             for (int i = 0; i < _trains.Count; i++)
-            {
-                if (_trains[i].Route.Distance > 0)
+            {                
+                if (_trains[i].GetDistance(_trains[i].Route) > 0)
                 {
-                    Console.WriteLine($"Поезд с маршрутом {_trains[i].Route.PointDeparture} - {_trains[i].Route.PointDeparture} в пути" +
-                    $" Осталось {_trains[i].Route.Distance} км до прибытия.");
+                    Console.WriteLine($"Поезд с маршрутом {_trains[i].Route.PointDeparture} - {_trains[i].Route.PointArrival} в пути" +
+                    $" Осталось {_trains[i].GetDistance(_trains[i].Route)} км до прибытия.");
                     _trains[i].Route.SkipDidistance();
-                    break;
                 }
                 else
                 {
-                    Console.WriteLine($"Поезд маршрутa {_trains[i].Route.PointDeparture} - {_trains[i].Route.PointDeparture} прибыл ");
-                }
-            }
-        }
-    }
-
-    class Train
-    {
-        private Route _route = new Route();
-        private Wagons _wagons = new Wagons();
-
-        public Route Route => _route;
-    }
-
-    class Route
-    {
-        private string _pointDeparture;
-        private string _pointArrival;
-        private int _distance;
-
-        public string PointDeparture => _pointDeparture;
-        public string PointArrival => _pointArrival;
-        public int Distance => _distance;
-
-        public Route()
-        {
-            GreateRoute();
-        }
-
-        private void GreateRoute()
-        {
-            Console.WriteLine("Укажите пункт отправления");
-            _pointDeparture = Console.ReadLine();
-            Console.WriteLine("Укадите пункт прибытия");
-            _pointArrival = Console.ReadLine();
-            Console.WriteLine("Установиь дистанция в км");
-            _distance = Convert.ToInt32(Console.ReadLine());
-        }
-
-        public void SkipDidistance()
-        {
-            _distance -= 10;           
-        }
-    }
-
-    class Wagons
-    {
-        private Passenger _passenger = new Passenger();
-        private string _standard = "стандартный вагон на 40 пассажиров";
-        private string _double = "двух уровневый вагон на 80 пассажиров";
-        private int _standartPassenger = 40;
-        private int _doublePassenger = 80;
-        private int _minNumber = 4;
-        private int _maxNumber = 16; 
-
-        public Wagons()
-        {
-            SetQuantity();
-        }
-
-        public void SetQuantity()
-        {
-            Console.WriteLine("Производим расчет количетсва вагонов для состава.");
-            
-            if (_standartPassenger * _maxNumber > _passenger.AllNumber)
-            {
-                for (int i = _minNumber; i < _maxNumber; i++)
-                {
-                    if (_standartPassenger * i > _passenger.AllNumber)
-                    {
-                        Console.WriteLine($"Для перевозки  {_passenger.AllNumber} требуется {i} {_standard}");
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                for (int i = _minNumber; i < _maxNumber; i++)
-                {
-                    if (_doublePassenger *i> _passenger.AllNumber)
-                    {
-                        Console.WriteLine($"Для перевозки  {_passenger.AllNumber} требуется {i} {_double}");
-                        break;
-                    }
+                    Console.WriteLine($"Поезд маршрутa {_trains[i].Route.PointDeparture} - {_trains[i].Route.PointArrival} прибыл ");
                 }
             }            
         }
-    }
 
-    class Passenger
-    {
-        private int _minNumber = 160;
-        private int _maxNumber = 1280;
-        private Random _random = new Random();
-        public int AllNumber { get; private set; }
-
-        public Passenger()
+        class Train
         {
-            AllNumber = _random.Next(_minNumber, _maxNumber);
+            private Route _route = new Route();
+            private Wagons _wagons = new Wagons();
+
+            public Route Route => _route;
+
+            public int GetDistance(Route route)
+            {
+                int distance = route.Distance;
+                return distance;
+            }
+        }
+
+        class Route
+        {
+            private string _pointDeparture;
+            private string _pointArrival;
+            private int _distance;
+
+            public string PointDeparture => _pointDeparture;
+            public string PointArrival => _pointArrival;
+            public int Distance => _distance;
+
+            public Route()
+            {
+                GreateRoute();
+            }
+
+            private void GreateRoute()
+            {
+                Console.WriteLine("Укажите пункт отправления");
+                _pointDeparture = Console.ReadLine();
+                Console.WriteLine("Укадите пункт прибытия");
+                _pointArrival = Console.ReadLine();
+                Console.WriteLine("Установиь дистанция в км");
+                bool completed = false;
+                int intValue = 0;
+
+                while (completed == false)
+                {
+                    string userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out intValue))
+                    {
+                        completed = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Не коректный ввод данных.");
+                    }
+                }
+
+                _distance = intValue;
+            }
+
+            public void SkipDidistance()
+            {
+                int subtract = 10;
+                _distance -= subtract;
+            }
+        }
+
+        class Wagons
+        {
+            private Passenger _passenger = new Passenger();
+            private string _titleStandard = "стандартный вагон";
+            private string _titleDouble = "двух уровневый вагон";
+            private int _standartPassenger = 40;
+            private int _doublePassenger = 80;
+            private int _minNumber = 4;
+            private int _maxNumber = 16;
+
+            public Wagons()
+            {
+                SetQuantity();
+            }
+
+            public void SetQuantity()
+            {
+                Console.WriteLine("Производим расчет количетсва вагонов для состава.");
+
+                if (_standartPassenger * _maxNumber > _passenger.AllNumber)
+                {
+                    for (int i = _minNumber; i < _maxNumber; i++)
+                    {
+                        if (_standartPassenger * i > _passenger.AllNumber)
+                        {
+                            Console.WriteLine($"Для перевозки  {_passenger.AllNumber} требуется {i} {_titleStandard}");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = _minNumber; i < _maxNumber; i++)
+                    {
+                        if (_doublePassenger * i > _passenger.AllNumber)
+                        {
+                            Console.WriteLine($"Для перевозки  {_passenger.AllNumber} требуется {i} {_titleDouble}");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        class Passenger
+        {
+            private int _minNumber = 160;
+            private int _maxNumber = 1280;
+            private Random _random = new Random();
+            public int AllNumber { get; private set; }
+
+            public Passenger()
+            {
+                AllNumber = _random.Next(_minNumber, _maxNumber);
+            }
         }
     }
 }
